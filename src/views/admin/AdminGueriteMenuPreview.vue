c25f5e93bea89d74f3601700c77582ab3fcf121c
Remy Zulauff <remy.zulauff@gmail.com>
2023-06-13 19:54:20 +0200

    feat(AdminGueriteMenuPreview.vue): add new component to preview guerite menu
    A new component was added to the project to preview the guerite menu. The component is called AdminGueriteMenuPreview and it imports the VGueriteMenu component to display the menu. The component receives a menu object as a prop.

---
+++ src/views/admin/AdminGueriteMenuPreview.vue
@@ -0,0 +1,12 @@
+<script setup>
+import VGueriteMenu from "@/components/VGueriteMenu.vue"
+const props = defineProps({
+  menu: Object,
+})
+</script>
+
+<template>
+  <div>
+    <VGueriteMenu :menu="props.menu" />
+  </div>
+</template>
