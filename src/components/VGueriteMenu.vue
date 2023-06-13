f48cba1706cca1ac1bc1f6c4b65f25923fc74a95
Remy Zulauff <remy.zulauff@gmail.com>
2023-06-13 19:51:39 +0200

    feat(VGueriteMenu.vue): create VGueriteMenu component to display menu items and prices with a sun icon separator

---
+++ src/components/VGueriteMenu.vue
@@ -0,0 +1,28 @@
+<script setup>
+import AppContent from "@/layouts/AppContent.vue"
+import { SunIcon } from "@heroicons/vue/outline"
+const props = defineProps({
+  menu: Object,
+})
+</script>
+
+<template>
+  <AppContent class="uppercase" :text-center="true">
+    <h2>{{ props.menu.title }}</h2>
+    <p class="italic">
+      {{ props.menu.text }}
+    </p>
+    <div class="not-prose">
+      <sun-icon class="h-5 w-5 mx-auto text-primary" aria-hidden="true" />
+    </div>
+    <template v-for="(section, idx) in menu.sections" :key="idx">
+      <p>
+        <span v-for="item in section.items" :key="item">{{ item }}</span>
+      </p>
+      <div class="not-prose">
+        <sun-icon class="h-5 w-5 mx-auto text-primary" aria-hidden="true" />
+      </div>
+    </template>
+    <p class="italic lowercase">{{ props.menu.price }}</p>
+  </AppContent>
+</template>
